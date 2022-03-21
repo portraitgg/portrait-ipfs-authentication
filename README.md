67bb860276b0f92a69bf8ab3fc2b6c7cafdaa76c
Ryan <ryanshahine@msn.com>
2022-03-21 14:59:43 +0100

    README, code clean-up.
    Code cleaning credits Giliam
    README added

---
+++ README.md
@@ -0,0 +1,31 @@
+# portrait-ipfs-authentication
+
+portrait-ipfs-authentication is a Node.js Express authentication layer for handling authentication by means of ``accessToken`` and ``ethAddress``. The layer acts as a gate between an IPFS node and the web such that no security and networking changes have to be made for the IPFS daemon.
+
+## Installation
+
+Use the package manager [npm](https://www.npmjs.com/) to install all packages.
+
+```bash
+npm install
+```
+
+## Usage
+
+```bash
+cd portrait-ipfs-authentication
+npm install
+node app.js
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## Credits
+- [Ryan Shahine](https://github.com/ryanshahine)
+- [Giliam Verheide](https://github.com/giliamverheide)
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 
 const { create } = require('ipfs-http-client');
-const ipfsHost = `${process.env.IPFS_HOST}`
+const ipfsHost = process.env.IPFS_HOST
 
 MongoClient = require('mongodb').MongoClient;
-const dbUrl = `${process.env.DB_AUTH}`;
-const dbName = `${process.env.DB_NAME}`;
+const dbUrl = process.env.DB_AUTH;
+const dbName = process.env.DB_NAME;
 const dbClient = new MongoClient(dbUrl);
 const dbConnect = dbClient.connect();
 
@@ -21,45 +21,56 @@ app.post("/", upload.single("data"), async function (req, res) {
 
     // Check if authentication is valid in the database
     const AuthValid = await dbConnect.then(() => {
-        const db = dbClient.db(dbName);
+        const db = dbClient.db(dbName)
 
-        return db.collection("user").findOne({ ethAddress: ethAddress, accessToken: accessToken }).then(function (result) {
-            return result ? true : false;
-        });
-    });
+        return db
+            .collection('user')
+            .findOne({ ethAddress, accessToken })
+            .then((result) => !!result)
+    })
 
     // Checking for missing fields or invalid authentication with AuthValid. If there are no missing fields and AuthValid is true, then the file is uploaded to IPFS.
     if (!req.file) {
         return res.status(400).json({ status: 400, message: 'Missing file' });
-    } else if (!accessToken) {
-        return res.status(400).json({ status: 400, message: 'Missing access token' });
-    } else if (!ethAddress) {
-        return res.status(400).json({ status: 400, message: 'Missing eth address' });
-    } else if (!AuthValid) {
-        return res.status(401).json({ status: 401, message: 'Invalid user authentication' });
-    } else {
-        try {
-            const ipfs = create({
-                host: ipfsHost,
-                port: '5001',
-                protocol: 'http',
-            });
-            const file = await ipfs.add(req.file.buffer);
-
-            return res
-                .status(200)
-                .json({
-                    status: 200,
-                    filename: req.file.originalname,
-                    hash: file.path,
-                    message: 'Succesfully uploaded',
-                });
-        } catch (e) {
-            return res.status(400).json({ status: 400, message: e.message });
-        }
     }
+
+    if (!accessToken) {
+        return res.status(400).json({ status: 400, message: 'Missing access token' });
+    }
+
+    if (!ethAddress) {
+        return res.status(400).json({ status: 400, message: 'Missing eth address' });
+    }
+
+    if (!AuthValid) {
+        return res.status(401).json({ status: 401, message: 'Invalid user authentication' });
+    }
+
+    try {
+        const ipfs = create({
+            host: ipfsHost,
+            port: '5001',
+            protocol: 'http',
+        });
+
+        const file = await ipfs.add(req.file.buffer);
+
+        return res
+            .status(200)
+            .json({
+                status: 200,
+                filename: req.file.originalname,
+                hash: file.path,
+                message: 'Succesfully uploaded',
+            });
+    } catch (e) {
+        return res.status(400).json({ status: 400, message: e.message });
+    }
+
 });
 
-app.listen(process.env.PORT || 1390, () => {
-    console.log(`Security authentication layer listening on port 1390`);
+const port = process.env.PORT || 1390
+
+app.listen(port, () => {
+    console.log(`Security authentication layer listening on port ${port}`);
 });
